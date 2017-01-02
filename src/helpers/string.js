b61b831ede3afa19c36c7af903e3272ce0e101cd
Vince Lee <vince.lee@polyhatsoftware.co.uk>
2017-01-02 21:34:10 +0000

    fix lpad

--- src/helpers/string.js
+++ src/helpers/string.js
@@ -2,5 +2,5 @@ export const lpad = (str, padChar = '0', maxLength = 2) => {
   str += ''
   return str.length >= maxLength
     ? str
-    : str + padChar.repeat(maxLength - str.length)
+    : padChar.repeat(maxLength - str.length) + str
 }
